b96c9b4f6a8567eb5890d1081a6e0a222625681e
c5apple <c5apple_party@yahoo.co.jp>
2023-01-22 14:44:32 +0900

    クエリパラメータ

--- README.md
+++ README.md
@@ -6,3 +6,22 @@
 http://localhost:5000/graphql
 
 `curl -X POST -H "Content-Type: application/json" http://localhost:5000/graphql`
+
+```
+curl -XPOST -H "Content-Type:application/graphql"  -d '
+ query getUser {
+   user(id: 3) {
+     name
+   }
+ }' http://localhost:5000/graphql
+```
+
+```
+curl -XPOST -H "Content-Type:application/graphql"  -d '
+ query getUser {
+   users {
+     name
+     sex
+   }
+ }' http://localhost:5000/graphql
+```
--- index.js
+++ index.js
@@ -4,14 +4,27 @@ const { buildSchema } = require('graphql')
 
 const app = express();
 
+const dummyPosts = [
+  { id: 1, name: '太郎', sex: 'male' },
+  { id: 2, name: '次郎', sex: 'male' },
+  { id: 3, name: '花子', sex: 'female' },
+]
+
 app.use('/graphql', graphqlHTTP({
   schema: buildSchema(`
     type Query {
-      message: String
+      user(id: Int!): Post
+      users: [Post]
+    },
+    type Post {
+      id: Int
+      name: String
+      sex: String
     }
   `),
   rootValue: {
-    message: () => 'Hello World!'
+    user: args => dummyPosts.filter(post => post.id === args.id)[0],
+    users: () => dummyPosts
   },
 
   graphiql: true
